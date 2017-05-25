fd6a4d66b382f08d6247e302b0eb9941e6a4c0c2
Raynold Ng <raynold.ng24@gmail.com>
2017-05-25 18:47:13 +0800

    Added CORS

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import PythonShell from 'python-shell';
+import cors from 'cors';
 
 let app = express(),
     port = process.env.PORT || 3001;
 let router = express.Router();
 app.server = http.createServer(app);
+
+// enable CORS
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.use(cors());
 
 router.get('/', (req, res) => {
   res.json({message: 'Hooray! Welcome to our API!'});
