843572ea10df25ff59289ec29a1f5f1ffdd3d3a4
Raynold Ng <raynold.ng24@gmail.com>
2017-07-17 15:39:07 +0800

    removed redundant tests and removed implicit timeout for test

--- test/modUtilsTest.js
+++ test/modUtilsTest.js
@@ -20,6 +20,7 @@ describe("Module Utilities", function() {
       expect(modUtils.timetableValid(candidateTimetable2)).to.equal(true);
     })
     it("False if there is a clash", function() {
+      this.timeout(0);
       candidateTimetable = ["MA1102R_Lecture_SL1", "MA1102R_Tutorial_T04", "MA1102R_Laboratory_B06", "CS1231_Tutorial_15", "CS1231_Sectional Teaching_2", "GER1000_Tutorial_S05", "MA1100_Tutorial_T06", "MA1100_Lecture_SL1"]
       expect(modUtils.timetableValid(candidateTimetable)).to.equal(false);
     })
